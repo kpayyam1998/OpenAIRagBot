da5cbef36ccde09017b1b3052ea744dfc72b7bbe
kpayyam1998 <karuppasamy12041998@gmail.com>
2024-10-18 17:05:53 +0530

    abstract code

---
+++ app/base.py
@@ -0,0 +1,17 @@
+from abc import ABC,abstractmethod
+
+class DocumentProcessor(ABC):
+
+    @abstractmethod
+    def ingest_document(self,*args, **kwargs):
+        pass
+
+class EmbeddingProcessor(ABC):
+    @abstractmethod
+    def embed_document(self, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def embed_query(self, *args, **kwargs):
+        pass
+
