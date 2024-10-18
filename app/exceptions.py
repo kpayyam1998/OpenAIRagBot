7ff2a5a9a1a876aa7edb964e8f002f3dd4fc2b1f
kpayyam1998 <karuppasamy12041998@gmail.com>
2024-10-18 17:03:24 +0530

    exception added

---
+++ app/exceptions.py
@@ -0,0 +1,8 @@
+class RAGBotException(Exception):
+    pass
+
+class ValidationError(RAGBotException):
+    pass
+
+class RetrievalError(RAGBotException):
+    pass
---
+++ app/validators.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel,HttpUrl,ValidationError  as PydanticValidationError
+from pydantic import ValidationError
+
+class DocumentIngestionRequest(BaseModel):
+    url: list[HttpUrl] # Expecting a list of urls
+
+class QueryRequest(BaseModel):
+    question : str # empty string
+
+def validationDocumentIngestion(data):
+    try:
+        DocumentIngestionRequest(**data)
+        #return True
+    except PydanticValidationError as e:
+        raise ValidationError(f"Document ingestion validation error: {e}")
+    
+def validationQueryRequest(data):
+    try:
+        QueryRequest(**data)
+        # return True
+    except PydanticValidationError as e:
+        raise ValidationError(f"Query validation error: {e}")
